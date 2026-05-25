f2c3e3ea011ca8e1322fb7584acfb6e8ad783cfd
semsettin <akyilsemsettin1@gmail.com>
2026-05-25 18:49:00 +0300

    update

--- public/favicon.jsx
+++ public/favicon.jsx
@@ -1,8 +1,15 @@
-<svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <rect width="64" height="64" rx="18" fill="#FFD21A"></rect>
-  
+<svg
+  width="64"
+  height="64"
+  viewBox="0 0 64 64"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <rect width="64" height="64" rx="18" fill="#FFD21A" />
+
   <g transform="rotate(35 32 32)">
-    <path d="M30.5 6
+    <path
+      d="M30.5 6
          C28.8 6 27.5 7.3 27.5 9V27.5
          L10.5 34
          C8.8 34.7 7.5 36.3 7.5 38.2V41
@@ -14,6 +21,8 @@
          L56.5 41V38.2
          C56.5 36.3 55.2 34.7 53.5 34
          L36.5 27.5V9
-         C36.5 7.3 35.2 6 33.5 6H30.5Z" fill="#8B0000"></path>
+         C36.5 7.3 35.2 6 33.5 6H30.5Z"
+      fill="#8B0000"
+    />
   </g>
-</svg>
+</svg>;
--- src/components/AnimatedSection.jsx
+++ src/components/AnimatedSection.jsx
@@ -3,10 +3,9 @@ import { motion } from "framer-motion";
 export default function AnimatedSection({ children, className = "" }) {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 36 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.18 }}
-      transition={{ duration: 0.65, ease: "easeOut" }}
+      animate={{ opacity: 1, y: 0 }}
+      initial={false}
+      transition={{ duration: 0 }}
       className={className}
     >
       {children}
